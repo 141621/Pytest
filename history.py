31aee1f964e913de49c6b468aa46a11a5e54e517
141621 <55832938+141621@users.noreply.github.com>
2019-12-12 16:32:01 +0100

    Initial commit

---
+++ history.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Thu Dec 12 12:26:52 2019)---
+runfile('C:/Users/Sondre/.spyder-py3/temp.py', wdir='C:/Users/Sondre/.spyder-py3')
---
+++ temp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x = 1
+i = 1
+how
+for i in range(6):
+     
+    print(x)
